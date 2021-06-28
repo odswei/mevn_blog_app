7f3a3a9f79aac611bdcb2ccc155e6b58878aa65c
odswei <antelepkecil@gmail.com>
2021-06-28 17:48:06 +0700

    adding toggle feature

--- is/src/views/Series.vue
+++ is/src/views/Series.vue
@@ -50,7 +50,7 @@ export default {
   },
   async created() {
     await axios
-      .get(`http://localhost:3000/series/${this.$route.params.id}/chapters`)
+      .get(`/series/${this.$route.params.id}/chapters`)
       .then(({ data }) => {
         this.series = data;
       });
